ce322aa791bbc65f5b997b88aebc0802568e0647
mohankumaraspire <mohankumar.sakthivel@aspiresys.com>
2023-05-09 20:13:14 +0530

    Ang-Crud-09

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialdesignModule } from '../materialdesign/materialdesign/materialdesign.module';
+import { DialogComponent } from './dialog/dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialdesignModule
   ],
   providers: [],
   bootstrap: [AppComponent]
